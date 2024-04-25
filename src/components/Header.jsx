316c2fc211545fa8a86d165a75ec3cf72c97a7f5
sini1004 <tlsgml656@gmail.com>
2024-04-25 15:18:22 +0900

    인트로 구조, 디자인 잡기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,68 @@
 import React, { useState } from "react";
 
 const headerNav = [
-    {
-        title: "intro",
-        url: "#intro",
-    },
-    {
-        title: "skill",
-        url: "#skill",
-    },
-    {
-        title: "site",
-        url: "#site",
-    },{
-        title: "protfile",
-        url: "#prot",
-    },{
-        title: "contact",
-        url: "#contact",
-    }
-]
+  {
+    title: "intro",
+    url: "#intro",
+  },
+  {
+    title: "skill",
+    url: "#skill",
+  },
+  {
+    title: "site",
+    url: "#site",
+  },
+  {
+    title: "protfile",
+    url: "#prot",
+  },
+  {
+    title: "contact",
+    url: "#contact",
+  },
+];
 
 const Header = () => {
-    const[show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const toggleMenu = () => {
-        setShow((prevShow) => !prevShow)
-    };
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  };
 
-
-    return (
-        <header id="header">
-            <div className="header__inner">
-                <div className="header__logo">
-                    <a href="/">portfolio<em>vite</em></a>
-                </div>
-                <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
-                    <ul>
-                        {headerNav.map((nav,key)=>(
-                            <li key={key}>
-                                <a href={nav.url}>{nav.title}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <div 
-                    className="header__nav__mobile" 
-                    id="headerToggle"
-                    aria-controls="primary-menu"
-                    aria-expanded={show ? "true" : "false"}
-                    onClick={toggleMenu}
-                    >
-                    <span></span>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header id="header">
+      <div className="header__inner">
+        <div className="header__logo">
+          <a href="/">
+            portfolio<em>vite</em>
+          </a>
+        </div>
+        <nav
+          className={`header__nav ${show ? "show" : ""}`}
+          role="navigation"
+          aria-label="메인 메뉴"
+        >
+          <ul>
+            {headerNav.map((nav, key) => (
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded={show ? "true" : "false"}
+          onClick={toggleMenu}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 const Intro = () => {
-    return <div>Intro</div>;
+  return (
+    <section id="intro">
+      <div className="intro__inner">
+        <h1 className="intro__title">port</h1>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro__text">
+          <div className="text">
+            <div>talent is</div>
+            <div>found at the end of the</div>
+            <div>effort</div>
+          </div>
+          <div className="img">
+            <img src="{about}" alt="어바웃" />
+          </div>
+        </div>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;

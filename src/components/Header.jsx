173ed3823a38d08d6e18dfcdb2d65b02c70e1909
sini1004 <tlsgml656@gmail.com>
2024-04-25 09:42:12 +0900

    헤더영역 1차 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },{
+        title: "protfile",
+        url: "#prot",
+    },{
+        title: "contact",
+        url: "#contact",
+    }
+]
 
 const Header = () => {
+    const[show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow)
+    };
+
+
     return (
         <header id="header">
             <div className="header__inner">
                 <div className="header__logo">
                     <a href="/">portfolio<em>vite</em></a>
                 </div>
-                <nav className="hader__nav">
+                <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
                     <ul>
-                        <li><a href="#">intro</a></li>
-                        <li><a href="#">skill</a></li>
+                        {headerNav.map((nav,key)=>(
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
-                <div className="header__nav__mobile" id="headerToggle">
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle"
+                    aria-controls="primary-menu"
+                    aria-expanded={show ? "true" : "false"}
+                    onClick={toggleMenu}
+                    >
                     <span></span>
                 </div>
             </div>

20b9395c08e481ac11f6dde7dd0668ba8752898d
sini1004 <tlsgml656@gmail.com>
2024-05-02 12:01:01 +0900

    사이트 구조, 디자인 잡기, 데이터 설정 및 완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Site = () => {
-    return <div>Site</div>;
+  return (
+    <section id="site">
+      <div className="site__inner">
+        <h2 className="site__title">site</h2>
+        <div className="site__wrap">
+          <article className="site__item s1">
+            <span className="num">1.</span>
+            <div className="text">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <h3 className="title">제작 사이트</h3>
+            <div className="btn">
+              <a href="">코드링크(깃)</a>
+              <a href="">뷰 링크(사이트)</a>
+            </div>
+            <div className="info">
+              <span>제목</span>
+              <span>설명</span>
+              <span>스택</span>
+            </div>
+          </article>
+          <article className="site__item s2">
+            <span className="num">2.</span>
+            <div className="text">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <h3 className="title">제작 사이트</h3>
+            <div className="btn">
+              <a href="">코드링크(깃)</a>
+              <a href="">뷰 링크(사이트)</a>
+            </div>
+            <div className="info">
+              <span>제목</span>
+              <span>설명</span>
+              <span>스택</span>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Site;

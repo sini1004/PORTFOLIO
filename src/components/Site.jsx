e3f79534e5c1ac7390d31736cbc103777a606545
sini1004 <tlsgml656@gmail.com>
2024-05-08 16:12:10 +0900

    사이트 코드 수정

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,47 +1,35 @@
 import React from "react";
 
+import { siteText } from "../constants";
+
 const Site = () => {
   return (
     <section id="site">
       <div className="site__inner">
-        <h2 className="site__title">site</h2>
+        <h2 className="site__title">
+          Site Coding <em>나의 작업물</em>
+        </h2>
         <div className="site__wrap">
-          <article className="site__item s1">
-            <span className="num">1.</span>
-            <div className="text">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <h3 className="title">제작 사이트</h3>
-            <div className="btn">
-              <a href="">코드링크(깃)</a>
-              <a href="">뷰 링크(사이트)</a>
-            </div>
-            <div className="info">
-              <span>제목</span>
-              <span>설명</span>
-              <span>스택</span>
-            </div>
-          </article>
-          <article className="site__item s2">
-            <span className="num">2.</span>
-            <div className="text">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <h3 className="title">제작 사이트</h3>
-            <div className="btn">
-              <a href="">코드링크(깃)</a>
-              <a href="">뷰 링크(사이트)</a>
-            </div>
-            <div className="info">
-              <span>제목</span>
-              <span>설명</span>
-              <span>스택</span>
-            </div>
-          </article>
+          {siteText.map((site, key) => (
+            <article className={`site__item s${key + 1}`} key={key}>
+              <span className="num">{key + 1}</span>
+              <div className="text">
+                <div>{site.text[0]}</div>
+                <div>{site.text[1]}</div>
+                <div>{site.text[2]}</div>
+              </div>
+              <h3 className="title">{site.title}</h3>
+              <div className="btn">
+                <a href={site.code}>code</a>
+                <a href={site.view}>view</a>
+              </div>
+              <div className="info">
+                <span>{site.info[0]}</span>
+                <span>{site.info[1]}</span>
+                <span>{site.info[2]}</span>
+              </div>
+            </article>
+          ))}
         </div>
       </div>
     </section>

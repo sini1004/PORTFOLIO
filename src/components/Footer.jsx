8c73ce46d68f5f70e4bc7614bd88f1b1b7f2a780
sini1004 <tlsgml656@gmail.com>
2024-05-08 10:48:11 +0900

    푸터 구조, 디자인 잡기 및 기본 세팅

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer id="footer" role="contentinfo">
+      <div className="footer__inner">
+        <div className="footer__text">
+          <span>sini</span>
+          <span>@web</span>
+        </div>
+        <div className="footer__info">
+          <div className="left">
+            <div className="title">
+              <a href="#">signup</a>
+            </div>
+            <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+          </div>
+          <div className="right">
+            <h3>social</h3>
+            <ul>
+              <li>
+                <a href="#"></a>
+                <em>문구</em>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer__right">© 2024</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

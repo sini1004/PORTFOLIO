589782b47f26b5a4cbea7a613f9e2bee92e994af
shinhee <tlsgml656@gmail.com>
2024-06-13 19:44:39 +0900

    백그라운드 이미지 추가, main figure 태그 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Main = ({ children }) => {
-    return (
-    <main id="main" role="main">
-        {children}
-    </main>
-    );
+	return (
+		// <main id="main" role="main">
+		//     {children}
+		// </main>
+		<main id="main" role="main" className="main-background">
+			{children}
+			<figure className="overlay"></figure>
+		</main>
+	);
 };
 
 export default Main;

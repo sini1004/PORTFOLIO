bb2573cfd738d16ab99e9f97288c6c3fb165b51e
shinhee <tlsgml656@gmail.com>
2024-10-27 22:25:55 +0900

    fix baseurl

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import HomeView from "./views/HomeView";
-import Site from "./components/Site";
-import ProjectDetail from "./page/ProjectDetail";
+import HomeView from './views/HomeView';
+import Site from './components/Site';
+import ProjectDetail from './page/ProjectDetail';
 
-import lenis from "./utils/lenis";
-import link from "./utils/link";
+import lenis from './utils/lenis';
+import link from './utils/link';
 
 const App = () => {
-  useEffect(() => {
-    lenis();
-    link();
-  }, []);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/site" element={<Site />} />
-        <Route path="/page-detail/:id" element={<ProjectDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	useEffect(() => {
+		lenis();
+		link();
+	}, []);
+	return (
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Routes>
+				<Route path="/" element={<HomeView />} />
+				<Route path="/site" element={<Site />} />
+				<Route path="/page-detail/:id" element={<ProjectDetail />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
